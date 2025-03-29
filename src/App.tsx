54a44088ce431f2004ce24d8bf32d51c4aac75a7
mihierkansal <mihierkansal@outlook.com>
2025-03-29 14:55:34 -0700

    Make UI pretty

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,64 @@
-import { createSignal, For } from "solid-js";
-import solidLogo from "./assets/solid.svg";
+import { createSignal, For, Show } from "solid-js";
 import viteLogo from "/vite.svg";
 import "./App.css";
+
+enum WeatherCondition {
+  Sunny = 1000,
+  PartlyCloudy = 1003,
+  Cloudy = 1006,
+  Overcast = 1009,
+  Mist = 1030,
+  PatchyRainPossible = 1063,
+  PatchySnowPossible = 1066,
+  PatchySleetPossible = 1069,
+  PatchyFreezingDrizzlePossible = 1072,
+  ThunderyOutbreaksPossible = 1087,
+  BlowingSnow = 1114,
+  Blizzard = 1117,
+  Fog = 1135,
+  FreezingFog = 1147,
+  PatchyLightDrizzle = 1150,
+  LightDrizzle = 1153,
+  FreezingDrizzle = 1168,
+  HeavyFreezingDrizzle = 1171,
+  PatchyLightRain = 1180,
+  LightRain = 1183,
+  ModerateRainAtTimes = 1186,
+  ModerateRain = 1189,
+  HeavyRainAtTimes = 1192,
+  HeavyRain = 1195,
+  LightFreezingRain = 1198,
+  ModerateOrHeavyFreezingRain = 1201,
+  LightSleet = 1204,
+  ModerateOrHeavySleet = 1207,
+  PatchyLightSnow = 1210,
+  LightSnow = 1213,
+  PatchyModerateSnow = 1216,
+  ModerateSnow = 1219,
+  PatchyHeavySnow = 1222,
+  HeavySnow = 1225,
+  IcePellets = 1237,
+  LightRainShower = 1240,
+  ModerateOrHeavyRainShower = 1243,
+  TorrentialRainShower = 1246,
+  LightSleetShowers = 1249,
+  ModerateOrHeavySleetShowers = 1252,
+  LightSnowShowers = 1255,
+  ModerateOrHeavySnowShowers = 1258,
+  LightShowersOfIcePellets = 1261,
+  ModerateOrHeavyShowersOfIcePellets = 1264,
+  PatchyLightRainWithThunder = 1273,
+  ModerateOrHeavyRainWithThunder = 1276,
+  PatchyLightSnowWithThunder = 1279,
+  ModerateOrHeavySnowWithThunder = 1282,
+}
 
 const API_KEY = "6da7994b31e7498bbaf142034252503";
 
 function getApiUrl(endpoint: string, additional: string) {
-  return `http://api.weatherapi.com/v1/${endpoint}?key=${API_KEY}` + additional;
+  return (
+    `https://api.weatherapi.com/v1/${endpoint}?key=${API_KEY}` + additional
+  );
 }
 
 function App() {
@@ -44,7 +96,7 @@ function App() {
       condition: {
         text: string;
         icon: string;
-        code: number;
+        code: WeatherCondition;
       };
       wind_mph: number;
       wind_kph: number;
@@ -155,118 +207,235 @@ function App() {
   }>();
 
   function getWeather() {
+    setWeather(undefined);
     fetch(getApiUrl("forecast.json", "&q=" + encodeURIComponent(coords())))
       .then((response) => response.json())
       .then((data) => {
         setWeather(data);
+        switch (data.current.condition.code) {
+          case WeatherCondition.Sunny:
+            console.log("Sunny");
+            if (data.current.is_day === 1) {
+              console.log("Sunny Day");
+              window.document.body.setAttribute("data-weather", "sunny-day");
+            } else {
+              window.document.body.setAttribute("data-weather", "clear-nite");
+            }
+            break;
+          case WeatherCondition.PartlyCloudy:
+            console.log("Partly Cloudy");
+            if (data.current.is_day === 1) {
+              window.document.body.setAttribute(
+                "data-weather",
+                "partly-cloudy-day"
+              );
+            } else {
+              window.document.body.setAttribute(
+                "data-weather",
+                "partly-cloudy-nite"
+              );
+            }
+            break;
+          case WeatherCondition.Cloudy:
+          case WeatherCondition.Overcast:
+          case WeatherCondition.Mist:
+          case WeatherCondition.Fog:
+          case WeatherCondition.FreezingFog:
+            console.log("Cloudy");
+            if (data.current.is_day === 1) {
+              window.document.body.setAttribute("data-weather", "cloudy");
+            } else {
+              window.document.body.setAttribute("data-weather", "cloudy");
+            }
+            break;
+          case WeatherCondition.HeavyRain:
+          case WeatherCondition.HeavyRainAtTimes:
+          case WeatherCondition.TorrentialRainShower:
+          case WeatherCondition.ModerateOrHeavyRainShower:
+          case WeatherCondition.LightRainShower:
+          case WeatherCondition.LightRain:
+          case WeatherCondition.ModerateRainAtTimes:
+          case WeatherCondition.ModerateRain:
+            window.document.body.setAttribute("data-weather", "rainy");
+
+            break;
+          case WeatherCondition.HeavySnow:
+          case WeatherCondition.PatchyHeavySnow:
+          case WeatherCondition.PatchyModerateSnow:
+          case WeatherCondition.LightSnow:
+          case WeatherCondition.ModerateSnow:
+          case WeatherCondition.PatchyLightSnow:
+          case WeatherCondition.ModerateOrHeavySnowShowers:
+          case WeatherCondition.LightSnowShowers:
+          case WeatherCondition.PatchyLightSnowWithThunder:
+          case WeatherCondition.ModerateOrHeavySnowWithThunder:
+            window.document.body.setAttribute("data-weather", "snowy");
+            break;
+          default:
+            console.log("Unknown");
+        }
         console.log(data);
       });
   }
 
+  function myLoc() {
+    setWeather(undefined);
+    const options = {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0,
+    };
+
+    function success(pos: GeolocationPosition) {
+      const crd = pos.coords;
+
+      console.log(`Latitude : ${crd.latitude}`);
+      console.log(`Longitude: ${crd.longitude}`);
+      setCoords(`${crd.latitude},${crd.longitude}`);
+      getWeather();
+    }
+
+    function error(err: GeolocationPositionError) {
+      console.warn(`ERROR(${err.code}): ${err.message}`);
+    }
+
+    navigator.geolocation.getCurrentPosition(success, error, options);
+  }
+  myLoc();
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://solidjs.com" target="_blank">
-          <img src={solidLogo} class="logo solid" alt="Solid logo" />
-        </a>
-      </div>
-      <h1>Vite + Solid</h1>
-      <div class="card">
-        <h3>LAT LON</h3>
-        <input id="lat" />
-        <input id="lon" />
+    <div class="weather-container">
+      <div class="weather-header">
         <button
           onClick={() => {
-            setCoords(`${lat.value},${lon.value}`);
-            getWeather();
+            myLoc();
           }}
         >
-          ENTER LATLON
+          ⮙
         </button>
-        <input
-          type="text"
-          value={search()}
-          onChange={(e) => {
-            setSearch(e.target.value);
-            fetch(
-              getApiUrl("search.json", "&q=" + encodeURIComponent(search()))
-            )
-              .then((response) => response.json())
-              .then((data) => setSearchResults(data));
-          }}
-        />
-        <For each={searchResults()}>
-          {(result) => (
+        <div class="search-container">
+          <input
+            placeholder="Search for a location"
+            type="text"
+            value={search()}
+            onInput={(e) => {
+              setSearch(e.target.value);
+              fetch(
+                getApiUrl("search.json", "&q=" + encodeURIComponent(search()))
+              )
+                .then((response) => response.json())
+                .then((data) => setSearchResults(data));
+            }}
+          />
+          <Show when={searchResults().length > 0}>
+            <div class="search-results">
+              <For each={searchResults()}>
+                {(result) => (
+                  <div
+                    onClick={() => {
+                      setCoords(`${result.lat},${result.lon}`);
+                      getWeather();
+                      setSearchResults([]);
+                      setSearch("");
+                    }}
+                  >
+                    {result.name}, {result.country} ({result.region})
+                  </div>
+                )}
+              </For>
+            </div>
+          </Show>
+        </div>
+      </div>
+      <Show
+        fallback={
+          <div
+            style={{
+              height: "50vh",
+              display: "flex",
+              "justify-content": "center",
+              "align-items": "center",
+            }}
+          >
+            Loading...
+          </div>
+        }
+        when={weather()}
+      >
+        <div class="weather-top">
+          <h1>{weather()?.current.temp_f}°F</h1>
+          <div>
             <div
-              onClick={() => {
-                setCoords(`${result.lat},${result.lon}`);
-                getWeather();
+              style={{
+                "font-size": "1.2rem",
               }}
             >
-              {result.name}, {result.country} ({result.region})
+              {weather()?.location.name}, {weather()?.location.region},{" "}
+              {weather()?.location.country}
             </div>
-          )}
-        </For>
-      </div>
-      <div class="read-the-docs">
-        <div>{weather()?.location.name}</div>
-        <div>{weather()?.current.temp_c}</div>
-        <div>{weather()?.current.condition.text}</div>
-        <div>
-          <img src={weather()?.current.condition.icon} alt="weather icon" />
-        </div>
-        <div>{weather()?.current.temp_f}</div>
-        <div>{weather()?.current.feelslike_c}</div>
-        <div>{weather()?.current.feelslike_f}</div>
-        <div>{weather()?.current.humidity}</div>
-        <div>{weather()?.current.wind_mph}</div>
-        <div>{weather()?.current.wind_kph}</div>
-        <div>{weather()?.current.wind_dir}</div>
-
-        <For each={weather()?.forecast.forecastday}>
-          {(day) => (
             <div>
-              <div>
-                {day.date}
-                <img src={day.day.condition.icon} alt="weather icon" />
-                {day.day.condition.text}
-                <br />
-                {day.day.maxtemp_c}
-                <br />
-                {day.day.mintemp_c}
-                <br />
-                {day.day.avgtemp_c}
-                <br />
-                {day.day.maxwind_mph}
-                <br />
-                {day.day.maxwind_kph}
-                <br />
-                {day.day.totalprecip_mm}
-                <br />
-                {day.day.totalprecip_in}
-                <br />
-                {day.day.totalsnow_cm}
-                <br />
-
-                <For each={day.hour}>
-                  {(hour) => (
-                    <div>
-                      {hour.time} {hour.temp_f}
-                      <img src={hour.condition.icon} alt="weather icon" />
-                      {hour.condition.text}
-                    </div>
-                  )}
-                </For>
-              </div>
+              H: {weather()?.forecast.forecastday[0].day.maxtemp_f}°F, L:{" "}
+              {weather()?.forecast.forecastday[0].day.mintemp_f}°F
             </div>
-          )}
-        </For>
-      </div>
-    </>
+            <div>Feels like: {weather()?.current.feelslike_f}°F</div>
+            <div>{weather()?.current.condition.text}</div>
+          </div>
+        </div>
+        <hr />
+        <div class="info-container">
+          <div class="info-card">
+            <h2>{weather()?.current.humidity}%</h2>
+            <div>humidity</div>
+          </div>
+          <div class="info-card">
+            <h2>{weather()?.current.wind_mph} mph</h2>
+            <div>wind speed</div>
+          </div>
+          <div class="info-card">
+            <h2>{weather()?.current.wind_dir}</h2>
+            <div>wind direction</div>
+          </div>
+          <div class="info-card">
+            <h2>{weather()?.current.precip_in} in</h2>
+            <div>precipitation</div>
+          </div>
+
+          <div class="info-card">
+            <h2>{weather()?.current.uv}</h2>
+            <div>uv index</div>
+          </div>
+
+          <div class="info-card">
+            <h2>{weather()?.current.vis_miles} mi</h2>
+            <div>visibility</div>
+          </div>
+        </div>
+
+        <hr />
+        <div class="hourly-forecast">
+          <For each={weather()?.forecast.forecastday[0].hour}>
+            {(hour) => (
+              <div>
+                <div>{hour.temp_f}°F</div>
+                <div>{getTime(hour.time)}</div>
+                <div>
+                  <img src={hour.condition.icon} alt="weather icon" />
+                </div>
+                <div>{hour.condition.text}</div>
+              </div>
+            )}
+          </For>
+        </div>
+      </Show>
+    </div>
   );
 }
 
 export default App;
+
+function getTime(time: string) {
+  const date = new Date(time);
+  return date.toLocaleTimeString("en-US", {
+    hour: "numeric",
+    minute: "2-digit",
+  });
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,13 @@
-import { defineConfig } from 'vite'
-import solid from 'vite-plugin-solid'
+import { defineConfig } from "vite";
+import solid from "vite-plugin-solid";
 
 export default defineConfig({
   plugins: [solid()],
-})
+  server: {
+    host: "0.0.0.0",
+    https: {
+      key: "./example.com+5-key.pem",
+      cert: "./example.com+5.pem",
+    },
+  },
+});
